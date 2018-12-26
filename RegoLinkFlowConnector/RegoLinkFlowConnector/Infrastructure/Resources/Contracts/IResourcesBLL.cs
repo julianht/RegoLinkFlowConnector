1baafac0d455260ee72b250334aeedf67cc88999
jhenao <jhenao@ITROIJHENAO>
2018-12-26 11:25:22 -0500

    Resources file included

---
+++ RegoLinkFlowConnector/RegoLinkFlowConnector/Infrastructure/Resources/Contracts/IResourcesBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegoLinkFlowConnector.Infrastructure.Resources.Contracts
+{
+    /// <summary>
+    /// Interface with resources logic
+    /// </summary>
+    public interface IResourcesBLL
+    {
+        /// <summary>
+        /// Gets the resource item by name
+        /// </summary>
+        /// <param name="name">Name of the item to recover</param>
+        /// <returns>Resource value associated to the name</returns>
+        string GetResource(string name);
+    }
+}
