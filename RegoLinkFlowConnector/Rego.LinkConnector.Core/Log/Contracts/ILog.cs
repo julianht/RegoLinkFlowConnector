bf7dd5c2fcf77fa07349a0576c0bbbb9e00db2a7
jhenao <jhenao@ITROIJHENAO>
2018-12-28 14:48:48 -0500

    Log implementation defined

---
+++ RegoLinkFlowConnector/Rego.LinkConnector.Core/Log/Contracts/ILog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rego.LinkConnector.Core.Log.Contracts
+{
+    /// <summary>
+    /// Contract for log definition
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        /// Writes a error message in the log
+        /// </summary>
+        /// <param name="message">Error message</param>
+        void WriteLogError(string message);
+
+        /// <summary>
+        /// Writes a info message in the log
+        /// </summary>
+        /// <param name="message">Info message</param>
+        void WriteLogInfo(string message);
+
+        /// <summary>
+        /// Writes a warning message in the log
+        /// </summary>
+        /// <param name="message">Warning message</param>
+        void WriteLogWarning(string message);
+    }
+}
